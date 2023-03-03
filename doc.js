48097262f5425b399fd01cf77d7bf54ed900ac41
Aleksandr <125996874+MrAlex-Neo@users.noreply.github.com>
2023-03-03 22:57:24 +0500

    Изменил с красивых массивов на некрасивые объекты

--- doc.js
+++ doc.js
@@ -1,14 +1,18 @@
 alert('Здесь вы можете сформировать список учащихся. Следуйте инструкции и все у вас будет тип топ. Улыбайтесь!');
 let amount = +prompt('Сколько человек в группе:');
 let students = [];
+let v = ''
 for (let i = 0; i < amount; i++){
     let name = prompt(`Имя ${i + 1} учащегося:`);
     let surName = prompt(`Фамилия ${i + 1} учащегося:`);
-    students.push([`${i + 1}. ${name} ${surName}\n`]);
+    students.push({
+        Имя: name,
+        Фамилия: surName
+    });
+    v+=`${i + 1}. ${name} ${surName}\n`
     }
 console.log(students);
-alert(`${students}`)
-
+alert(v)
 
 
 
